415a6fd46924658c8476794622571ad5ff6d08fd
mhkeller <michael.keller@gmail.com>
2013-10-30 00:33:59 -0400

    with banquo published, now link it as a proper module

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var banquo = require('../banquo/src/banquo.js');
+var banquo = require('banquo');
 var config = require('./config.json');
 
 var app = express();
